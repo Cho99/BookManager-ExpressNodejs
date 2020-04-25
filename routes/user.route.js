133a428eec136a85a0247bda678f9c49ae8cba3e
Glitch (middleware-lesson13-codersx) <none>
2020-04-25 13:53:45 +0000

    🏪👂 Updated with Glitch

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require("../controllers/user.controller");
+const validate = require("../validates/user.validate");
 
 router.get("/", controller.index);
 
 router.get("/create", controller.getCreate);
 
-router.post("/create", controller.postCreate);
+router.post("/create", validate.postCreate ,controller.postCreate);
 
 router.get("/view/:id", controller.view);
 
---
+++ validates/user.validate.js
@@ -0,0 +1,26 @@
+module.exports.postCreate = (req, res, next) => {
+    let name = req.body.name;
+  let phone = req.body.phone;
+  let errors = [];
+  
+  if(name.length > 30) {
+    errors.push("Tên người dùng không được quá 30 ký tự");
+  }
+  if(phone.length > 15) {
+    errors.push("Không phải số điện thoại");
+  }
+  if(!name) {
+    errors.push("Name is required");
+  }
+  if(!phone) {
+    errors.push("Phone is required");
+  }
+  if (errors.length) {
+    res.render("users/create", {
+      errors,
+      value: req.body
+    });
+    return;
+  }
+  next();
+}
